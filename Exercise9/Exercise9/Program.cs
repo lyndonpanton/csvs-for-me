e9acee2d7c79ab134d580ac1c32401bb43aa87ba
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-07 23:14:58 +0000

    Completed problem 2
    Retrieved the second section of the supplied string

--- Exercise9/Exercise9/Program.cs
+++ Exercise9/Exercise9/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            // Problem 1 – Extracting the Pyramid Slot Number
+
             // format: <pyramid slot number>,<block letter>,<whether or not the block should be lit>
             Console.Write("Enter a valid 3 part string: ");
             string userInput = Console.ReadLine();
@@ -12,7 +14,18 @@
 
             int pyramidSlotNumber = int.Parse(userInput.Substring(0, firstCommaInput));
 
-            Console.WriteLine(pyramidSlotNumber);
+            Console.WriteLine("First section of the input: " + pyramidSlotNumber);
+
+            // Problem 2 – Extracting the Block Letter
+
+            string twoPartInput = userInput.Substring(firstCommaInput + 1);
+
+            int secondCommaInput = twoPartInput.IndexOf(",");
+
+            char blockLetter = char.Parse(twoPartInput.Substring(0, secondCommaInput));
+
+            Console.WriteLine("Second section of the input: " + blockLetter);
+
         }
     }
 }
