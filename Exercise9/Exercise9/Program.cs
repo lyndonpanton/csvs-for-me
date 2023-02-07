5ff4e9c02c8368be150df1f67612e4d02b7a469d
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-07 23:10:36 +0000

    Completed problem 1
    Retrieved the first section of the supplied string

--- Exercise9/Exercise9/Program.cs
+++ Exercise9/Exercise9/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // format: <pyramid slot number>,<block letter>,<whether or not the block should be lit>
+            Console.Write("Enter a valid 3 part string: ");
+            string userInput = Console.ReadLine();
+
+            int firstCommaInput = userInput.IndexOf(',');
+
+            int pyramidSlotNumber = int.Parse(userInput.Substring(0, firstCommaInput));
+
+            Console.WriteLine(pyramidSlotNumber);
         }
     }
 }
